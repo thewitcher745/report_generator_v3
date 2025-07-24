14d200bba7b8cf99fdc556d466659eb78c36fdc5
Mohammad Amin Seifinia <thewitcher745@gmail.com>
2025-07-24 12:56:58 +0330

    FEAT: get_template now also saves the QR and referrals
    - Instead of only the name of the template which isn't very useful

--- tg/handler_functions/get_template.py
+++ tg/handler_functions/get_template.py
@@ -3,11 +3,21 @@ This file gets the template from the user through a callback query.
 """
 
 from tg.handler_functions.helpers.conversation_stages import END
+from tg.handler_functions.helpers.templates import get_template as get_template_details
 
 
 async def get_template(update, context):
     await update.callback_query.answer()
 
     context.user_data["template"] = update.callback_query.data
+
+    template_details = get_template_details(
+        exchange=context.user_data["exchange"], template_name=update.callback_query.data
+    )
+
+    context.user_data["qr_code"], context.user_data["referral_code"] = (
+        template_details["qr"],
+        template_details["referral"],
+    )
 
     return END
