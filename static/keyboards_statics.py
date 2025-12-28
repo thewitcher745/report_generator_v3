00db91d8bde463fbf40b329a51a065db2d84bc00
Mohammad Amin Seifinia <thewitcher745@gmail.com>
2025-12-28 19:48:33 +0330

    FEAT: Added binance_1 and binance_2

--- static/keyboards_statics.py
+++ static/keyboards_statics.py
@@ -11,8 +11,8 @@ EXCHANGE_LIST = [
 
 IMAGE_LIST = {
     "binance": [
-        # {"label": "Binance 1", "callback_query": "binance_1", "image_list_label": 1},
-        # {"label": "Binance 2", "callback_query": "binance_2", "image_list_label": 2},
+        {"label": "Binance 1", "callback_query": "binance_1", "image_list_label": 1},
+        {"label": "Binance 2", "callback_query": "binance_2", "image_list_label": 2},
         # {"label": "Binance 3", "callback_query": "binance_3", "image_list_label": 3},
         # {"label": "Binance 4", "callback_query": "binance_4", "image_list_label": 4},
         # {"label": "Binance 5", "callback_query": "binance_5", "image_list_label": 5},
